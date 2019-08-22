04378c79e0c72e9392fda37909e921cdc0632757
lily <wl09720@ly.com>
2019-08-22 17:48:58 +0800

    updte

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <div>HelloWorld</div>
-    <div class="clip-path"></div>
   </div>
 </template>
 
@@ -10,16 +9,6 @@
   font-size: 32px;
   padding: 40px;
   text-align: center;
-}
-.clip-path {
-  width: 100px;
-  height: 100px;
-  background-color: red;
-  /* -webkit-clip-path: polygon(50% 0px, 100% 100%, 0px 100%); */
-  /* -webkit-clip-path: polygon(0px 50%, 50% 0px, 100% 50%, 50% 100%, 0px 50%); */
-  -webkit-clip-path: polygon(0% 60%, 20% 0%, 60% 0%, 40% 60%, 0% 60%);
-  /* -webkit-clip-path: polygon(50% 0px, 100% 100%, 0px 100%); */
-  /* -webkit-clip-path: polygon(50% 0px, 100% 100%, 0px 100%); */
 }
 </style>
 
@@ -55,10 +44,12 @@ export default {
     initData() {
       let Chord = new BaaS.TableObject("books");
       let query = new BaaS.Query();
-    //   Chord.find();
-      Chord.setQuery(query).find().then(res => {
-          console.log(res)
-      })
+      //   Chord.find();
+      Chord.setQuery(query)
+        .find()
+        .then(res => {
+          console.log(res);
+        });
     }
   }
 };
