46e94b906f6a29a8c5b90a7b8901042fd0f09803
lily <wl09720@ly.com>
2020-07-22 09:01:22 +0800

    es6tutorial

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,8 @@
-#!/usr/bin/env sh
 
-# 终止一个错误
-set -e
+# 构建打包
+yarn build
 
-# 构建
-npm run build
+# 发布surge
+surge -d seya-es6tutorial.surge.sh -p dist/
 
-# 进入生成的构建文件夹
-# cd docs/.vuepress/dist
-cd dist
 
-# 如果你是要部署到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果你想要部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:seYaO/seYaO.github.io.git master
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:seYaO/site.git master:gh-pages
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
